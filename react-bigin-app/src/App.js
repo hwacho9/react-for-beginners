306f0583dbfdab8f34951e70c655fb1afd039024
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2023-03-07 21:53:38 +0900

    React Router

--- react-bigin-app/src/App.js
+++ react-bigin-app/src/App.js
@@ -1,8 +1,17 @@
 import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
-
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+//Link 는 브라우저 새로고침 없이도 유저를 다른 페이지로 이동 시켜주는 컴포넌트
 function App() {
-    return null;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movie" element={<Detail />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
